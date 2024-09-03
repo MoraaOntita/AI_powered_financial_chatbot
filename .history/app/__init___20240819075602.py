49cd35de5fb4ec3b0450f62288c28c13f4d08d40
MoraaOntita <moraaontita1844@gmail.com>
2024-09-03 06:32:44 +0300

    Created a new file init in the models folder, modified the makefile to have the updated path of my csv file, modified the app/__init__.py to use the correct path for the configuration file

---
+++ .history/app/__init___20240819075602.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    
+    with app.app_context():
+        from . import routes  # Import routes module
+
+    return app
