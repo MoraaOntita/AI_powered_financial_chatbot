2a23462a5ab26089dd5fb3cb04d2d6abb5c4ec80
MoraaOntita <moraaontita1844@gmail.com>
2024-09-12 07:04:33 +0300

    Modified the port to 5432 from 5433 and the host from localhost to postgres

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ DB_CONFIG = {
     'dbname': os.getenv('POSTGRES_DB'),
     'user': os.getenv('POSTGRES_USER'),
     'password': os.getenv('POSTGRES_PASSWORD'),
-    'host': 'localhost',  # Assuming you're connecting to localhost
-    'port': 5433           # Use the port mapping from your docker-compose file
+    'host': 'postgres',  # Assuming you're connecting to localhost
+    'port': 5432           
 }
