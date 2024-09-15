6938932aa74f66d12c560adc3ecedc23ea5e78d3
MoraaOntita <moraaontita1844@gmail.com>
2024-09-15 06:48:38 +0300

    Modified the entrypoint.sh, docker-compose.yml and the index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Wait for PostgreSQL to be ready
-./wait-for-postgres.sh postgres "python src/inserting_data/insert_financial_data.py --data-file /app/data/BCG_X_Sheet1.csv && python src/inserting_data/insert_qa_pairs.py && exec flask run --host=0.0.0.0 --port=5000"
+./wait-for-postgres.sh postgres
+
+# Check if the wait was successful
+if [ $? -ne 0 ]; then
+  echo "PostgreSQL is not ready. Exiting."
+  exit 1
+fi
 
 # Run the data insertion scripts
 python src/inserting_data/insert_financial_data.py --data-file /app/data/BCG_X_Sheet1.csv
