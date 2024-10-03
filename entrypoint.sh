fa7c633cba8d215ec439edfee699b5c97d14319b
MoraaOntita <moraaontita1844@gmail.com>
2024-10-03 20:03:46 +0300

    Modified Mounting the .env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,28 +5,9 @@ set -a
 source /app/.env
 set +a
 
-# Wait for PostgreSQL to be ready
-/app/wait-for-postgres.sh postgres
-
-
-echo "PostgreSQL is up. Running data insertion scripts..."
-
-# Run the financial data insertion script
-python /app/src/inserting_data/insert_financial_data.py --data-file /app/data/BCG_X_Sheet1.csv
-if [ $? -ne 0 ]; then
-  echo "Financial data insertion failed. Exiting."
-  exit 1
-fi
-echo "Financial data inserted successfully."
-
-# Run the QA pairs insertion script
-python /app/src/inserting_data/insert_qa_pairs.py
-if [ $? -ne 0 ]; then
-  echo "QA pairs data insertion failed. Exiting."
-  exit 1
-fi
-echo "QA pairs inserted successfully."
-
-# Start the Flask application
-echo "Starting the Flask application..."
-exec flask run --host=0.0.0.0 --port=5000
+# Wait for PostgreSQL to be ready and then run the data insertion scripts and start Flask
+/app/wait-for-postgres.sh postgres "
+  python /app/src/inserting_data/insert_financial_data.py --data-file /app/data/BCG_X_Sheet1.csv &&
+  python /app/src/inserting_data/insert_qa_pairs.py &&
+  flask run --host=0.0.0.0 --port=5000
+"
