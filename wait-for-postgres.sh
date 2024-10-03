609d5379959a239dd6b86cad354ea573c1234691
MoraaOntita <moraaontita1844@gmail.com>
2024-10-03 15:23:27 +0300

    Increased the number of retries

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -13,8 +13,8 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 echo "Waiting for PostgreSQL to be ready..."
 until psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 2  # Adjust sleep time between retries if needed
 done
 
 >&2 echo "Postgres is up - executing command"
-exec "$cmd" 
+exec "$cmd"
