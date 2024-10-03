6bb17fc0b06b1f46e027c890d6f18a1927edf29b
MoraaOntita <moraaontita1844@gmail.com>
2024-10-03 20:14:55 +0300

    Modify the  entrypoint.sh to pass the entire command as a single string to wait-for-postgres.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ source /app/.env
 set +a
 
 # Wait for PostgreSQL to be ready and then run the data insertion scripts and start Flask
-/app/wait-for-postgres.sh postgres "
+/app/wait-for-postgres.sh postgres "bash -c '
   python /app/src/inserting_data/insert_financial_data.py --data-file /app/data/BCG_X_Sheet1.csv &&
   python /app/src/inserting_data/insert_qa_pairs.py &&
   flask run --host=0.0.0.0 --port=5000
-"
+'"
